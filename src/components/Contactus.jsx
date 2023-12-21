82270081b607442afe90cf10a6a81cc588ac71ea
Mohammed Aman Mev <mohammedaman0731@gmail.com>
2023-12-21 11:40:20 +0530

    ---

--- src/components/Contactus.jsx
+++ src/components/Contactus.jsx
@@ -12,13 +12,11 @@ const Contactus = () => {
       <div className="w-[100%] h-[100%] md:px-[100px] md:py-[50px] flex items-center justify-between sm:flex-wrap">
         <div className="bg-gray-800 flex items-center justify-center h-[500px] mt-[20px] mx-auto w-[100%] md:w-[450px] md:h-[550px]">
           <div>
-            
+            <h2>Hours of Operations</h2>
           </div>
         </div>
         <div className="bg-gray-800 flex items-center justify-center h-[500px] mt-[20px] mx-auto w-[100%] md:w-[450px] md:h-[550px]">
-          <div>
-            
-          </div>
+          <div></div>
         </div>
       </div>
     </div>
